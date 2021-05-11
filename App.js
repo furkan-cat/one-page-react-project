66093fbfc8142d0fb0c113be49db4b770b720bfe
Furkan Çat <75113766+InsomniumFerum@users.noreply.github.com>
2021-05-11 22:58:38 +0300

    Add files via upload
    Committed responsive design

---
+++ App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+import Main from "./components/Main/Main";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
