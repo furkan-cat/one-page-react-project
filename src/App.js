28b6f0f5c1524c3830200e40a931efc9c7f1f866
Furkan Çat <75113766+InsomniumFerum@users.noreply.github.com>
2021-05-12 01:29:12 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
 import "./App.css";
+import Posts from "./components/Posts/Posts";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Main />
+      <Posts />
     </div>
   );
 }
